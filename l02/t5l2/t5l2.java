537dbb5eaed1eea4f7c1b2434b213f3af8cf0788
Yaroslav <teslenko.yaroslav21@gmail.com>
2024-08-13 23:56:05 +0200

    урок 2 задание 5

--- l02/t5l2/t5l2.java
+++ l02/t5l2/t5l2.java
@@ -2,6 +2,6 @@ package l02.t5l2;
 
 public class t5l2 {
     public static void main(String[] args) {
-        System.out.println("Area of circle: "+ MyArea.areaOfCircle(45));
+        System.out.println("Area of circle: " + MyArea.areaOfCircle(45));
     }
 }
