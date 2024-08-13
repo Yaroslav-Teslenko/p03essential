f46d1ad864f3de1206ea9686c87d4fc5370acb7f
Yaroslav <teslenko.yaroslav21@gmail.com>
2024-08-13 23:48:17 +0200

    урок 2 задание 5

---
+++ l02/t5l2/MyArea.java
@@ -0,0 +1,8 @@
+package l02.t5l2;
+
+public class MyArea {
+    static final double PI = 3.14;
+    static public double areaOfCircle(double r){
+        return PI*r*r;
+    }
+}
---
+++ l02/t5l2/t5l2.java
@@ -0,0 +1,7 @@
+package l02.t5l2;
+
+public class t5l2 {
+    public static void main(String[] args) {
+        System.out.println("Area of circle: "+ MyArea.areaOfCircle(45));
+    }
+}
